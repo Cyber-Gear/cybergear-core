a39142c136e7b2ece607b72a83ed3e17c22ea8a3
havenquan <havenquan@qq.com>
2022-05-24 01:22:02 +0800

    cs

---
+++ contracts/token/interface/ICS.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.12;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+
+/**
+ * @title CS Interface
+ * @author ISEKAI-TEAM
+ * @notice Interface of the CS
+ */
+abstract contract ICS is IERC721Enumerable {
+    mapping(uint256 => mapping(string => uint256)) public data;
+    mapping(uint256 => mapping(string => uint256[])) public datas;
+
+    function spawnCs(uint256 hero, address to)
+        external
+        virtual
+        returns (uint256);
+
+    function setData(
+        uint256 csId,
+        string memory slot,
+        uint256 _data
+    ) external virtual;
+
+    function setDatas(
+        uint256 csId,
+        string memory slot,
+        uint256[] memory _datas
+    ) external virtual;
+
+    function safeTransferFromBatch(
+        address from,
+        address to,
+        uint256[] memory tokenIds
+    ) external virtual;
+
+    function getDatas(uint256 csId, string memory slot)
+        external
+        view
+        virtual
+        returns (uint256[] memory);
+
+    function tokensOfOwnerBySize(
+        address user,
+        uint256 cursor,
+        uint256 size
+    ) external view virtual returns (uint256[] memory, uint256);
+
+    function getRandomNumber(
+        uint256 csId,
+        string memory slot,
+        uint256 base,
+        uint256 range
+    ) external pure virtual returns (uint256);
+}
